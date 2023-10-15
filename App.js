45529e7a7018873e52375e52e06f690c1ee58607
şöhrett <sohretturaman@gmail.com>
2023-10-15 14:58:43 +0300

    removed tsx extentions

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View>
+      <Text>start page </Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
